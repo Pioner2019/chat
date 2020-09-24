94ae012ddee6c28b015a996188f69fcbf41bd0ea
Gleb Stepanov <tripolskiy.gleb@gmail.com>
2020-09-24 14:17:46 +0300

    qwerty

---
+++ plugins/pluginMakeLS.js
@@ -0,0 +1,81 @@
+
+   PP.pluginMakeLS = function(func, menuLS) {
+
+         let forma = document.createElement("div");
+             forma.id = "lichkaMakeMale";
+             forma.className = '';
+                  let tr = document.createElement("textarea");
+                      tr.id = 'tr';
+                      tr.rows = '5';
+                      tr.cols = '27';
+                      tr.placeholder = "введите сообщение здесь";
+                  forma.appendChild(tr);
+                  let bt = document.createElement("button");
+                      bt.id = 'bt';
+                      bt.innerHTML = 'Отправить';
+                  forma.appendChild(bt);
+                  let closeWindow = document.createElement("button");
+                      closeWindow.id = 'closeWindow';
+                      closeWindow.innerHTML = 'Закрыть окно';
+
+                      closeWindow.addEventListener("click", function() {
+                           forma.className = '';
+                      });
+
+                  forma.appendChild(closeWindow);
+         body.appendChild(forma);
+
+//-----------------------------------------------------------------------------------
+
+         let lichka = document.createElement("div");
+         lichka.id = 'lichka';
+         lichka.className = "";
+
+            let p1 = document.createElement("p");
+                p1.innerHTML = '&nbsp;&nbsp;У вас с этим участником пока нет диалога.';
+            lichka.appendChild(p1);
+
+            let p2 = document.createElement("p");
+                p2.innerHTML = '&nbsp;&nbsp;Хотите начать с ним диалог?';
+            lichka.appendChild(p2);
+
+            let p3 = document.createElement("p");
+                p3.innerHTML = '&nbsp;&nbsp;Если да -';
+                p3.style.marginBottom = '10px';
+            lichka.appendChild(p3);
+
+            let tar = document.createElement("textarea");
+                tar.id = 'dialogs';
+                tar.placeholder = 'Введите текст сообщения';
+                tar.cols = '26';
+                tar.rows = '4';
+            lichka.appendChild(tar);
+
+            let button = document.createElement("button");
+                button.id = 'button';
+                button.innerHTML = "Отправить";
+            lichka.appendChild(button);
+
+            let stopLS = document.createElement("button");
+                stopLS.id = 'stopLS';
+                stopLS.innerHTML = "Закрыть окно";
+                stopLS.addEventListener("click", funcStopLS);
+            lichka.appendChild(stopLS);
+
+         body.appendChild(lichka);
+
+         function funcStopLS() {
+             userLS.addEventListener("click", func);
+                 lichka.className = '';
+                 let menuLS = document.querySelector("#menuLS");
+                 menuLS.className = "";
+              setTimeout(function() {
+                lichka.innerHTML = "";
+                lichka.remove();
+                stopLS.remove();
+                menuLS.remove();
+                forma.remove();
+              }, 700);
+         }
+
+    }
---
+++ plugins/pluginRenderMessages.js
@@ -0,0 +1,29 @@
+
+   PP.pluginRenderMessages = function(baza, obj) {     // Получаем сюда из основного модуля обьект,
+     let otvetMessage = document.createElement("div");   // содержащий заголовок "От такого-то"(message.a),
+         otvetMessage.className = "otvetMessage";       // сам текст ответного послания(message.b),
+         let zagolovok = document.createElement("div");   // и цвет его сообщений(message.c), см. ниже...
+             zagolovok.classList.add("zagolovok");
+    //         zagolovok.id = 'zagolovok';
+             zagolovok.innerHTML = obj.a + ":";  // СООБЩЕНИЕ(message) представляет собой контейнер - оболочку
+         otvetMessage.appendChild(zagolovok);       //  otvetMessage, содержащий три компонента:
+                                                   // 1. zagolovok. В нём находится имя отправителя сообщения...
+         let telo = document.createElement("div"); // 2. telo: блок вариативной высоты(height: auto), поскольку
+    //         telo.id = 'telo';                     // сообщения бывают разного обьёма...
+             telo.classList.add("telo");
+             telo.innerHTML = obj.b;
+         otvetMessage.appendChild(telo);
+
+         let strTimeOtvet = document.createElement("div"); //... и 3. strTimeOtvet: это подвал, футер, содержащий
+    //         strTimeOtvet.id = 'strTimeOtvet';             // дату и время поступления сообщения.
+             strTimeOtvet.classList.add("strTimeOtvet");
+             let objTime = PP.pluginTimeClient();
+             strTimeOtvet.innerHTML = objTime.a;
+         otvetMessage.appendChild(strTimeOtvet);
+         otvetMessage.style.backgroundColor = obj.c; //...и отрисовываем у себя на
+//         otvetMessage.addEventListener("click", funcResponse); // странице его сообщение.
+     baza.appendChild(otvetMessage);
+     // lichka.insertAdjacentHTML(`beforeEnd`, `<button id='stopLS'>Закрыть личку</button>`);
+     baza.scrollTop = baza.scrollHeight;
+     return otvetMessage;
+    }
